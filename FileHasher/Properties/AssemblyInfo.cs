424e892aa0ab7e1b8c9f06197c112266e05f89d2
Daniil Shipilin <daniil.shipilin@gmail.com>
2020-04-19 11:23:58 +0200

    FileHasher v0.1.3

--- FileHasher/Properties/AssemblyInfo.cs
+++ FileHasher/Properties/AssemblyInfo.cs
@@ -4,4 +4,4 @@ using System.Reflection;
 [assembly: AssemblyDescription("FileHasher")]
 [assembly: AssemblyProduct("FileHasher")]
 [assembly: AssemblyCopyright("Daniil Shipilin")]
-[assembly: AssemblyVersion("0.1.2")]
+[assembly: AssemblyVersion("0.1.3")]
